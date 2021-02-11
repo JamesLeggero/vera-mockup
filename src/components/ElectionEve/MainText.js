27bf461d4b5bd681dc6701e543e4d1f1414ba5c6
JamesLeggero <james.m.leggero@gmail.com>
2021-02-11 10:32:20 -0500

    couple style adjustments

--- src/components/ElectionEve/MainText.js
+++ src/components/ElectionEve/MainText.js
@@ -16,9 +16,9 @@ const MainText = () => {
         </div>
         <Container className='mainText'>
         <h1>Voters of Color on Election Eve</h1>
-        <p>This year, Vera participated in the <a target= '_blank' rel="noreferrer" href='https://electioneve2020.com/poll/#/en/demographics/black/'>American Election Eve Poll</a> to learn more about the vote choices and views of thousands of voters nationwide, especially voters of color and voters in battleground states. This data tells a powerful story about the importance of voters of colors, whose views and political power will shape justice issues for years to come.</p>
+        <p>This year, Vera participated in the <a target= '_blank' rel="noreferrer" href='https://electioneve2020.com/poll/#/en/demographics/black/'>American Election Eve Poll</a> to learn more about the vote choices and views of thousands of voters nationwide, especially voters of color and voters in battleground states. This data tells a powerful story about the importance of voters of colors, whose views and political power will shape issues of justice, economy, and democracy for years to come. Their voices will be heard.</p>
         <p> In particular, over 80% of Black voters nationwide support reducing funding for law enforcement and <a target= '_blank' rel="noreferrer" href='https://naacp.org/latest/2020-american-election-eve-poll-finds-coronavirus-pandemic-and-racial-justice-among-most-important-issues-for-african-american-voters/'>investing more in
-social services and programs proven to reduce violence</a>, and overwhemingly believe in a reduction of the number of people in jails and prisons, as well as allowing people who are incarcerated to use Pell Grants to advance their education and training. At Vera, we are working with local community leaders nationwide to organize and empower their neighborhoods and communities.</p>
+social services and programs proven to reduce violence</a>, and overwhemingly believe in a reduction of the number of people in jails and prisons, as well as allowing people who are incarcerated to use Pell Grants to advance their education and training. At Vera, we are working with community leaders nationwide to organize and empower local communities.</p>
         </Container>
         </>
     )
