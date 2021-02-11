81ce30108c971f1b28d678ab8022f44e7ad3e57d
JamesLeggero <james.m.leggero@gmail.com>
2021-02-11 00:52:39 -0500

    changed bs import AGAIN

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import ReactDOM from 'react-dom';
 
 
 import App from './App';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css';
+
 import reportWebVitals from './reportWebVitals';
 
 
